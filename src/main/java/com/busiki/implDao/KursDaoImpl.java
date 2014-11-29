e2f1dcfbbd1ddf827330d88906ab35ebc4101c74
agdula <agdula15@gmail.com>
2014-11-29 16:16:06 +0100

    searchcontroller

---
+++ src/main/java/com/busiki/implDao/KursDaoImpl.java
@@ -0,0 +1,10 @@
+package com.busiki.implDao;
+
+import org.springframework.stereotype.Repository;
+
+import com.busiki.model.Kurs;
+
+@Repository
+public class KursDaoImpl extends AbstractDaoImpl<Kurs> {
+	
+}
